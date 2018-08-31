703efeeb1a62b78219f3d2dec6d9c65932ae5131
xHype <radumazilu24@gmail.com>
2018-08-31 15:32:28 +0300

    Properly update ports

--- TrackLiveWebApp/server.js
+++ TrackLiveWebApp/server.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var http = require("http");
-var port = process.env.port || 1337;
+var port = process.env.port || 8080;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Test\n');
+    res.end('Welcome to TrackLiveApp!\n');
 }).listen(port);
 //# sourceMappingURL=server.js.map
