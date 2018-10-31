1f78f2ddce3468afc35a0f435d1ae2774900ce23
xHype <radumazilu24@gmail.com>
2018-10-31 18:35:49 +0200

    Finalize post location route

--- TrackLiveWebApp/controllers/routes.ts
+++ TrackLiveWebApp/controllers/routes.ts
@@ -1,6 +1,10 @@
 ﻿import { app } from "../server";
+import { parseDate } from "tough-cookie";
 
 var datastore = require('../database');
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.get('/trackings', getAllTrackingInfo);
 
@@ -47,9 +51,8 @@ function postTrackingInfo(req, res) {
     // The kind for the new entity
     const kind = 'locations';
     // The name/ID for the new entity
-    const id = '12345';
+    const id = 1000000000000000 + Math.floor(Math.random() * Math.floor(89999999999999));
     // The Cloud Datastore key for the new entity
-
     const taskKey = datastore.key({
         namespace: "TrackLiveStorage",
         path: [kind, id]
@@ -59,16 +62,40 @@ function postTrackingInfo(req, res) {
     const task = {
         key: taskKey,
         data: {
-            Name: 'Radu'
+            Name: 'Location',
+            CreatedAt: new Date().toLocaleString(),
+            bearing: req.body.bearing,
+            latitude: parseFloat(req.body.latitude),
+            longitude: parseFloat(req.body.longitude),
+            speed: parseInt(req.body.speed),
+            time: new Date().toLocaleString(),
+            trackingId: req.params.trackingCode
         },
     };
 
     datastore
         .save(task)
         .then(() => {
-            console.log(`Saved ${task.key.name}: ${task.data.Name}`);
+            res.json({ "message": "Location successfully added!", "id": id });
         })
         .catch(err => {
-            console.error('ERROR:', err);
+            res.status(404).json({ "error": "100", "message": "Unable to add a location with the specified parameters." });
+        });
+}
+
+app.get('/trackings/:trackingCode/location', getTrackingLocation);
+
+function getTrackingLocation(req, res) {
+    console.log(req.params.trackingCode);
+    const query = datastore.createQuery('trackings').filter('__key__', '=', datastore.key(['trackings', req.params.trackingCode]));
+
+    datastore
+        .runQuery(query)
+        .then(results => {
+            const task = results[0][0];
+            res.json({ "id": req.params.trackingCode, "name": task.Name });
+        })
+        .catch(err => {
+            res.status(404).json({ "error": "100", "message": "Unable to find any trackingCode with the specified ID." });
         });
 }
